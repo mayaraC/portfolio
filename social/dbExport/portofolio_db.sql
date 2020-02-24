c46e819f044f6eeec9a2804127c7332db573a362
mayaraC <mayara.cchrd@eduge.com>
2020-02-24 13:35:13 +0100

    ajout de la base de données modifiée

--- social/dbExport/portofolio_db.sql
+++ social/dbExport/portofolio_db.sql
@@ -2,60 +2,43 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
-DROP SCHEMA IF EXISTS `portefolio_db` ;
-CREATE SCHEMA IF NOT EXISTS `portefolio_db` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
+CREATE SCHEMA IF NOT EXISTS `portefolio_db` DEFAULT CHARACTER SET utf8 ;
 USE `portefolio_db` ;
 
 -- -----------------------------------------------------
 -- Table `portefolio_db`.`post`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `portefolio_db`.`post` ;
-
 CREATE TABLE IF NOT EXISTS `portefolio_db`.`post` (
-  `idPost` INT NOT NULL AUTO_INCREMENT,
+  `idPost` INT(11) NOT NULL AUTO_INCREMENT,
   `commentaire` VARCHAR(255) NOT NULL,
-  `creationDate` TIMESTAMP NOT NULL,
-  `modificationDate` TIMESTAMP NULL,
+  `creationDate` TIMESTAMP NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp,
+  `modificationDate` TIMESTAMP NULL DEFAULT NULL,
   PRIMARY KEY (`idPost`))
-ENGINE = InnoDB;
+ENGINE = InnoDB
+AUTO_INCREMENT = 55
+DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
 -- Table `portefolio_db`.`media`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `portefolio_db`.`media` ;
-
 CREATE TABLE IF NOT EXISTS `portefolio_db`.`media` (
-  `idMedia` INT NOT NULL AUTO_INCREMENT,
+  `idMedia` INT(11) NOT NULL AUTO_INCREMENT,
   `typeMedia` VARCHAR(45) NOT NULL,
   `nomMedia` VARCHAR(45) NOT NULL,
-  `creationDate` TIMESTAMP NOT NULL,
-  `modificationDate` TIMESTAMP NULL,
-  PRIMARY KEY (`idMedia`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `portefolio_db`.`contenir`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `portefolio_db`.`contenir` ;
-
-CREATE TABLE IF NOT EXISTS `portefolio_db`.`contenir` (
-  `post_idPost` INT NOT NULL,
-  `media_idMedia` INT NOT NULL,
-  INDEX `fk_contenir_post1_idx` (`post_idPost` ASC),
-  INDEX `fk_contenir_media1_idx` (`media_idMedia` ASC),
-  CONSTRAINT `fk_contenir_post1`
+  `creationDate` TIMESTAMP NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp,
+  `modificationDate` TIMESTAMP NULL DEFAULT NULL,
+  `post_idPost` INT(11) NOT NULL,
+  PRIMARY KEY (`idMedia`),
+  INDEX `fk_media_post` (`post_idPost` ASC),
+  CONSTRAINT `fk_media_post`
     FOREIGN KEY (`post_idPost`)
     REFERENCES `portefolio_db`.`post` (`idPost`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_contenir_media1`
-    FOREIGN KEY (`media_idMedia`)
-    REFERENCES `portefolio_db`.`media` (`idMedia`)
-    ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+AUTO_INCREMENT = 31
+DEFAULT CHARACTER SET = utf8;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
